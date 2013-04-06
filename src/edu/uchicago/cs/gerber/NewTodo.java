e6493d66cd74ce6c4b585e01fdaad03ed8a7ed95
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 22:13:17 -0500

    p0

--- src/edu/uchicago/cs/gerber/NewTodo.java
+++ src/edu/uchicago/cs/gerber/NewTodo.java
@@ -13,7 +13,7 @@ import android.widget.EditText;
 import edu.uchicago.cs.gerber.db.TodosDataSource;
 import edu.uchicago.cs.gerber.model.Todo;
 
-public class NewTodo extends Activity implements OnClickListener {
+public class NewTodo extends Activity  {
 
 	Button btnAdd;
 	EditText edtTitle, edtDetail;
@@ -22,57 +22,10 @@ public class NewTodo extends Activity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.insert);
-		edtTitle = (EditText) findViewById(R.id.edtTitle);
-		edtDetail = (EditText) findViewById(R.id.edtDetail);
 
-		btnAdd = (Button) findViewById(R.id.btnUpdate);
-		btnAdd.setOnClickListener(this);
 
 	}
 
-	// ################################################
-	// used for ActionBar
-	// ################################################
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.actbar_insert, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.add:
-			addTodo();
-			break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	//################################################
-	// used for the button "add"
-	//################################################
-	@Override
-	public void onClick(View v) {
-		addTodo();
-
-	}
-
-	private void addTodo() {
-
-		Todo tdoAdd = null;
-		//Todo has a 2-arg construtor which leaves id as 0
-		tdoAdd = new Todo(edtTitle.getText().toString(), edtDetail.getText()
-				.toString());
-		Intent itnReturn = new Intent();
-		//add a serializable object (a Todo) to the intent
-		itnReturn.putExtra(Main.TODO_KEY, tdoAdd);
-		//set result to return from startActivityForResult (the itnReturn contains the Todo inside its bundle)
-		setResult(Main.REQ_INSERT, itnReturn);
-		//destroy this activity
-		finish();
-
-	}
 
 }
--- src/edu/uchicago/cs/gerber/UpdateTodo.java
+++ src/edu/uchicago/cs/gerber/UpdateTodo.java
@@ -14,7 +14,7 @@ import edu.uchicago.cs.gerber.db.TodosDataSource;
 import edu.uchicago.cs.gerber.model.Todo;
 
 
-public class UpdateTodo extends Activity implements OnClickListener {
+public class UpdateTodo extends Activity  {
 
 	Button btnUpdate, btnDelete;
 	EditText edtTitle, edtDetail;
@@ -25,94 +25,10 @@ public class UpdateTodo extends Activity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.update);
-		edtTitle = (EditText) findViewById(R.id.edtTitle);
-		edtDetail = (EditText) findViewById(R.id.edtDetail);
 
-		btnUpdate = (Button) findViewById(R.id.btnUpdate);
-		btnUpdate.setOnClickListener(this);
-
-		btnDelete = (Button) findViewById(R.id.btnDelete);
-		btnDelete.setOnClickListener(this);
-
-		// get the intent that was passed into this Activity
-		Intent itn = getIntent();
-		// extract the Todo that was put into the bundle of that intent
-		tdo = (Todo) itn.getSerializableExtra(Main.TODO_KEY);
-
-		edtTitle.setText(tdo.getTitle());
-		edtDetail.setText(tdo.getDetail());
 
 	}
 
-	// ################################################
-	// used for ActionBar
-	// ################################################
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.actbar_update, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-
-		case R.id.delete:
-			delTodo();
-			break;
-
-		case R.id.update:
-			updateTodo();
-			break;
-
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
-	// ################################################
-	// used for buttons "delete" and "update"
-	// ################################################
-	@Override
-	public void onClick(View v) {
-
-		switch (v.getId()) {
-		case R.id.btnDelete:
-			delTodo();
-			break;
-
-		case R.id.btnUpdate:
-			updateTodo();
-			break;
-
-		default:
-			break;
-		}
-
-	}
-
-	private void delTodo() {
-
-		// pass the original Todo back with a request to delete it.
-		Intent itnReturn = new Intent();
-		itnReturn.putExtra(Main.TODO_KEY, tdo);
-		setResult(Main.REQ_DELETE, itnReturn);
-		finish();
-
-	}
-
-	private void updateTodo() {
-		// pass the updated Todo back with a request to update it.
-		Intent itnReturn = new Intent();
-		// create/assign a new Todo based on any updated title or detail data
-		tdo = new Todo(tdo.getId(), edtTitle.getText().toString(), edtDetail
-				.getText().toString());
-		itnReturn.putExtra(Main.TODO_KEY, tdo);
-		setResult(Main.REQ_UPDATE, itnReturn);
-		finish();
-
-	}
 
 }
